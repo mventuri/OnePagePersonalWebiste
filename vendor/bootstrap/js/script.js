c59eceef876bee3724dbdf0fb52bde15a81f353d
mventuri <mventuri@hotmail.it>
2021-01-24 19:31:31 +0100

    first commit

---
+++ vendor/bootstrap/js/script.js
@@ -0,0 +1,23 @@
+$("#social").hide();
+$(".img-fluid").hide();
+
+$.ajax({
+  type: "GET",
+  url: "https://www.marcoventuritest.it/wooTest/wp-json/wp/v2/pages/1691",
+  dataType: "json",
+  success: function(respJson) {
+    var article = respJson.content.rendered;
+    var title = respJson.title.rendered;
+    $(".spinner-grow").remove();
+    $("#title").append(title);
+    $(".lead").append(article);
+    $("#social").show();
+    $(".img-fluid").show();
+  },
+  error: function() {
+    $(".lead").append(
+      "<div class='alert alert-danger' role='alert'>Ops! Something went wrong. Please try again.</div>"
+    );
+    $(".spinner-grow").remove();
+  }
+});
